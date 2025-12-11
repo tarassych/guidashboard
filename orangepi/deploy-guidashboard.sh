e14ae8c8192064fb81242b72eec90ac8c4927159
tarassych <taras.sych@gmail.com>
2025-12-11 21:10:41 +0200

    built and trying to load

--- orangepi/deploy-guidashboard.sh
+++ orangepi/deploy-guidashboard.sh
@@ -61,5 +61,5 @@ if [ "$LOCAL" = "$BASE" ]; then
   exit 0
 fi
 
-echo "Local repo state unusual (ahead/diverged). Not auto-updating."
+echo "Local repo state unusual (ahead/diverged). Not auto-updating. Exiting."
 exit 0
